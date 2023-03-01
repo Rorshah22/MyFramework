9e22c5697a0c502e930a49b687860b50acbdfc8f
turbok <postmaster@turbok.by>
2023-03-01 11:12:49 +0300

    fix: style css

--- templates/main/main.php
+++ templates/main/main.php
@@ -11,29 +11,35 @@
         <?php if ($key === 3) {
             break;
         } ?>
-<div class="card-img">
-        <p class="card-theme">Theme</p>
-        <div class="profile-data">
-            <img src="/../img/no-photo.png" alt="фото профиля" width="40px" height="40px">
-            <div class="profile-data-info">
-            <p><?= $article->getAuthor()->getNickname()?></p>
-            <p><?= $article->getCreatedAt()?></p>
+        <div class="card-img">
+            <p class="card-theme">Theme</p>
+            <div class="profile-data">
+                <img src="/../img/no-photo.png" alt="фото профиля" width="40px" height="40px">
+                <div class="profile-data-info">
+                    <p><?= $article->getAuthor()->getNickname() ?></p>
+                    <p><?= $article->getCreatedAt() ?></p>
+                </div>
             </div>
+            <h2 class="card-h2"><?= $article->getName() ?></h2>
+            <p><img class="card-image-background" src="/../img/news1.png" alt=""></p>
         </div>
-        <h2 class="card-h2"><?= $article->getName() ?></h2>
-        <p><img class="card-image-background" src="/../img/news1.png" alt=""></p>
-</div>
         <p class="card-text"><?php if (mb_strlen($article->getText()) < 100): ?>
                 <?= htmlentities($article->getText()) ?>
             <?php else: ?>
                 <?= mb_substr(htmlentities($article->getText()), 0, 250) . ' ...' ?>
             <?php endif; ?>
             <br>
-            <a href="/articles/<?= $article->getId() ?>">Читать далее</a>
+            <a class="arrow-1-btn" href="/articles/<?= $article->getId() ?>">Читать далее
+                <span class="arrow-1">
+                <span></span>
+            </span>
+        </a>
+
         </p>
 
         <?php if ($user !== null && ($user->isAdmin() || $user->getId() === $article->getAuthorId())): ?>
-            <p class="card-edit"><a class="card-edit-a" href="/articles/<?= $article->getId() ?>/edit">Редактировать</a></p>
+            <p class="card-edit"><a class="card-edit-btn"
+                                    href="/articles/<?= $article->getId() ?>/edit">Редактировать</a></p>
         <?php endif; ?>
         <?php if (count($articles) - 1 !== $key): ?>
 
